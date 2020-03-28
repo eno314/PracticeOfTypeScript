d81e72edb62831392f68515474d810d3d20fc882
Hiroto Kitamura <kirikonknow@gmail.com>
2020-03-28 14:15:54 +0900

    入力値の形式が不正な場合の実装とテストコードを追加

--- src/atcoder/beginners_selection/practiceAInputParser.test.ts
+++ src/atcoder/beginners_selection/practiceAInputParser.test.ts
@@ -2,11 +2,51 @@ import { PracticeAInput } from './PracticeAInput'
 import { practiceAInputParser } from './practiceAInputParser'
 
 describe('入力値を、整数a,b,cと文字列sにパースする', () => {
-  test.each([
-    ['1\n2 3\ntest', new PracticeAInput(1, 2, 3, 'test')],
-    ['72\n128 256\nmyonmyon', new PracticeAInput(72, 128, 256, 'myonmyon')]
-  ])('正常系', (inputString: string, expected: PracticeAInput) => {
-    const actual = practiceAInputParser(inputString)
-    expect(actual).toEqual(expected)
+  describe('正常系', () => {
+    test.each([
+      ['1\n2 3\ntest', new PracticeAInput(1, 2, 3, 'test')],
+      ['72\n128 256\nmyonmyon', new PracticeAInput(72, 128, 256, 'myonmyon')]
+    ])('a,b,c,sを持つPracticeAInputを返す', (inputString: string, expected: PracticeAInput) => {
+      const actual = practiceAInputParser(inputString)
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  describe('入力値の形式が不正の場合', () => {
+    test.each([
+      [''],
+      ['1\n2 3']
+    ])('入力値を改行コードで分割して3つ未満なら例外を投げる', (inputString: string) => {
+      expect(() => {
+        practiceAInputParser(inputString)
+      }).toThrowError('input format is invalid.')
+    })
+
+    test.each([
+      ['\n2 3\ntest'],
+      ['a\n2 3\ntest']
+    ])('1行目要素が数字ではないなら例外を投げる', (inputString: string) => {
+      expect(() => {
+        practiceAInputParser(inputString)
+      }).toThrowError('input format is invalid.')
+    })
+
+    test.each([
+      ['1\n2\ntest'],
+      ['1\n\ntest']
+    ])('2行目の要素が半角スペースで分割して2つ未満なら例外を投げる', (inputString: string) => {
+      expect(() => {
+        practiceAInputParser(inputString)
+      }).toThrowError('input format is invalid.')
+    })
+
+    test.each([
+      ['1\nb 3\ntest'],
+      ['1\n2 c\ntest']
+    ])('2行目の要素を半角スペースで分割して、分割した2つの要素のどちらかが数字ではないなら例外を投げる', (inputString: string) => {
+      expect(() => {
+        practiceAInputParser(inputString)
+      }).toThrowError('input format is invalid.')
+    })
   })
 })
--- src/atcoder/beginners_selection/practiceAInputParser.ts
+++ src/atcoder/beginners_selection/practiceAInputParser.ts
@@ -1,13 +1,37 @@
 import { PracticeAInput } from './PracticeAInput'
 
 export function practiceAInputParser (inputString: string): PracticeAInput {
+  const parsed = parseInputStringToArray(inputString)
+  return new PracticeAInput(parsed[0], parsed[1], parsed[2], parsed[3])
+}
+
+function parseInputStringToArray (inputString: string): [number, number, number, string] {
   const inputStrings = inputString.split('\n')
-  const bc = inputStrings[1].split(' ')
+  if (inputStrings.length < 3) {
+    throw new Error('input format is invalid.')
+  }
 
   const a = parseInt(inputStrings[0])
+  if (Number.isNaN(a)) {
+    throw new Error('input format is invalid.')
+  }
+
+  const bc = parseBAndCToArray(inputStrings[1])
+
+  return [a, bc[0], bc[1], inputStrings[2]]
+}
+
+function parseBAndCToArray (bcString: string): [number, number] {
+  const bc = bcString.split(' ')
+  if (bc.length < 2) {
+    throw new Error('input format is invalid.')
+  }
+
   const b = parseInt(bc[0])
   const c = parseInt(bc[1])
-  const s = inputStrings[2]
+  if (Number.isNaN(b) || Number.isNaN(c)) {
+    throw new Error('input format is invalid.')
+  }
 
-  return new PracticeAInput(a, b, c, s)
+  return [b, c]
 }
