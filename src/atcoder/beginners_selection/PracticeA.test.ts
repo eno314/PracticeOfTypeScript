ef3735faa5a5306cdebda12b566b2bf1bda443aa
Hiroto Kitamura <kirikonknow@gmail.com>
2020-03-28 16:38:20 +0900

    提出しやすいように1ファイルにまとめる

--- src/atcoder/beginners_selection/PracticeA.test.ts
+++ src/atcoder/beginners_selection/PracticeA.test.ts
@@ -1,37 +1,129 @@
-import { PracticeA } from './PracticeA'
-import { PracticeAInput } from './PracticeAInput'
+import { PracticeA, PracticeAInput, practiceAInputParser } from './PracticeA'
 
-jest.mock('./PracticeAInput')
-
-describe('execute', () => {
-  const outputString = 'output string'
-  const inputString = 'input string'
-
-  const mockInputLoader = jest.fn(() => inputString)
-  const mockInputParser = jest.fn(_ => new PracticeAInput(0, 0, 0, 'test'))
+describe('PracticeA 結合テスト', () => {
   const mockOutputPrinter = jest.fn(_ => _)
 
-  beforeAll(() => {
-    (PracticeAInput as jest.Mock).mockImplementation(() => {
-      return {
-        toOutputString: (): string => {
-          return outputString
-        }
-      }
-    })
-  })
+  test.each([
+    ['1\n2 3\ntest', '6 test'],
+    ['72\n128 256\nmyonmyon', '456 myonmyon']
+  ])('入力値を読み込み、入力値をパースして、出力値に変換し、出力する', (inputString: string, expectedOutputString: string) => {
+    const mockInputLoader = jest.fn(() => inputString)
+    const practiceA = new PracticeA(mockInputLoader, practiceAInputParser, mockOutputPrinter)
 
-  test('入力値を読み込み、入力値をパースして、出力値に変換し、出力する', () => {
-    const practiceA = new PracticeA(mockInputLoader, mockInputParser, mockOutputPrinter)
     practiceA.execute()
 
     // verify mockInputLoader
     expect(mockInputLoader.mock.calls.length).toBe(1)
-    // verify mockInputParser
-    expect(mockInputParser.mock.calls.length).toBe(1)
-    expect(mockInputParser.mock.calls[0][0]).toBe(inputString)
     // verify mockOutputPrinter
     expect(mockOutputPrinter.mock.calls.length).toBe(1)
-    expect(mockOutputPrinter.mock.calls[0][0]).toBe(outputString)
+    expect(mockOutputPrinter.mock.calls[0][0]).toBe(expectedOutputString)
+  })
+})
+
+describe('PracticeAInput ユニットテスト', () => {
+  describe('整数a,b,c と文字列s を出力値に変換する', () => {
+    test.each([
+      [new PracticeAInput(1, 2, 3, 'test'), '6 test'],
+      [new PracticeAInput(72, 128, 256, 'myonmyon'), '456 myonmyon']
+    ])('a+b+cとsを空白区切りで1行にする', (input: PracticeAInput, expected: string) => {
+      const actual = input.toOutputString()
+      expect(actual).toBe(expected)
+    })
+  })
+})
+
+describe('practiceAInputParser ユニットテスト', () => {
+  describe('入力値を、整数a,b,cと文字列sにパースする', () => {
+    describe('正常系', () => {
+      test.each([
+        ['1\n2 3\ntest', new PracticeAInput(1, 2, 3, 'test')],
+        ['72\n128 256\nmyonmyon', new PracticeAInput(72, 128, 256, 'myonmyon')]
+      ])('a,b,c,sを持つPracticeAInputを返す', (inputString: string, expected: PracticeAInput) => {
+        const actual = practiceAInputParser(inputString)
+        expect(actual).toEqual(expected)
+      })
+    })
+
+    describe('入力値の形式が不正の場合、例外を投げる', () => {
+      test.each([
+        [''],
+        ['1\n2 3']
+      ])('入力値を改行コードで分割して3つ未満', (inputString: string) => {
+        expect(() => {
+          practiceAInputParser(inputString)
+        }).toThrowError('input format is invalid.')
+      })
+
+      test.each([
+        ['\n2 3\ntest'],
+        ['a\n2 3\ntest']
+      ])('1行目要素が数字ではない', (inputString: string) => {
+        expect(() => {
+          practiceAInputParser(inputString)
+        }).toThrowError('input format is invalid.')
+      })
+
+      test.each([
+        ['1\n2\ntest'],
+        ['1\n\ntest']
+      ])('2行目の要素が半角スペースで分割して2つ未満', (inputString: string) => {
+        expect(() => {
+          practiceAInputParser(inputString)
+        }).toThrowError('input format is invalid.')
+      })
+
+      test.each([
+        ['1\nb 3\ntest'],
+        ['1\n2 c\ntest']
+      ])('2行目の要素を半角スペースで分割して、分割した2つの要素のどちらかが数字ではない', (inputString: string) => {
+        expect(() => {
+          practiceAInputParser(inputString)
+        }).toThrowError('input format is invalid.')
+      })
+    })
+
+    describe('a,b,c,sが制約に合っていない場合、例外を投げる', () => {
+      test.each([
+        ['0\n2 3\ntest'],
+        ['-1\n2 3\ntest'],
+        ['1.1\n2 3\ntest'],
+        ['1001\n2 3\ntest']
+      ])('aが1以上1000以下の整数ではない', (inputString: string) => {
+        expect(() => {
+          practiceAInputParser(inputString)
+        }).toThrowError('a violates constraint.')
+      })
+
+      test.each([
+        ['1\n0 3\ntest'],
+        ['1\n-2 3\ntest'],
+        ['1\n2.2 3\ntest'],
+        ['1\n1002 3\ntest']
+      ])('bが1以上1000以下の整数ではない', (inputString: string) => {
+        expect(() => {
+          practiceAInputParser(inputString)
+        }).toThrowError('b violates constraint.')
+      })
+
+      test.each([
+        ['1\n2 0\ntest'],
+        ['1\n2 -3\ntest'],
+        ['1\n2 3.3\ntest'],
+        ['1\n2 1003\ntest']
+      ])('cが1以上1000以下の整数ではない', (inputString: string) => {
+        expect(() => {
+          practiceAInputParser(inputString)
+        }).toThrowError('c violates constraint.')
+      })
+
+      test.each([
+        ['1\n2 3\n'],
+        ['1\n2 3\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa']
+      ])('sの文字列の長さが1以上100以下ではない', (inputString: string) => {
+        expect(() => {
+          practiceAInputParser(inputString)
+        }).toThrowError('s violates constraint.')
+      })
+    })
   })
 })
--- src/atcoder/beginners_selection/PracticeA.ts
+++ src/atcoder/beginners_selection/PracticeA.ts
@@ -1,5 +1,3 @@
-import { PracticeAInput } from './PracticeAInput'
-
 export class PracticeA {
   private readonly inputLoader: () => string
   private readonly inputParser: (input: string) => PracticeAInput
@@ -20,4 +18,86 @@ export class PracticeA {
     const input = this.inputParser(inputString)
     this.outputPrinter(input.toOutputString())
   }
+}
+
+export class PracticeAInput {
+  private readonly a: number
+  private readonly b: number
+  private readonly c: number
+  private readonly s: string
+
+  constructor (a: number, b: number, c: number, s: string) {
+    if (!this.isValidNumberProperty(a)) {
+      throw new Error('a violates constraint.')
+    }
+    if (!this.isValidNumberProperty(b)) {
+      throw new Error('b violates constraint.')
+    }
+    if (!this.isValidNumberProperty(c)) {
+      throw new Error('c violates constraint.')
+    }
+    if (!this.isValidStringProperty(s)) {
+      throw new Error('s violates constraint.')
+    }
+
+    this.a = a
+    this.b = b
+    this.c = c
+    this.s = s
+  }
+
+  private isValidNumberProperty (num: number): boolean {
+    if (!Number.isInteger(num)) {
+      return false
+    }
+
+    return num >= 1 && num <= 1000
+  }
+
+  private isValidStringProperty (str: string): boolean {
+    return str.length >= 1 && str.length <= 100
+  }
+
+  toOutputString (): string {
+    return `${this.a + this.b + this.c} ${this.s}`
+  }
+}
+
+export function practiceAInputParser (inputString: string): PracticeAInput {
+  const parsed = parseInputStringToArray(inputString)
+  return new PracticeAInput(parsed[0], parsed[1], parsed[2], parsed[3])
+}
+
+function parseInputStringToArray (inputString: string): [number, number, number, string] {
+  const inputStrings = inputString.split('\n')
+  if (inputStrings.length < 3) {
+    throw new Error('input format is invalid.')
+  }
+
+  const a = parseNumberString(inputStrings[0])
+  const bc = parseBAndCToArray(inputStrings[1])
+
+  return [a, bc[0], bc[1], inputStrings[2]]
+}
+
+function parseBAndCToArray (bcString: string): [number, number] {
+  const bc = bcString.split(' ')
+  if (bc.length < 2) {
+    throw new Error('input format is invalid.')
+  }
+
+  const b = parseNumberString(bc[0])
+  const c = parseNumberString(bc[1])
+  return [b, c]
+}
+
+function parseNumberString (numberString: string): number {
+  if (numberString.length === 0) {
+    throw new Error('input format is invalid.')
+  }
+  const num = Number(numberString)
+  if (Number.isNaN(num)) {
+    throw new Error('input format is invalid.')
+  }
+  return num
 }
