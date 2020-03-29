f0a8f83534ae311c06c2746b822eef23c6ed8cc1
Hiroto Kitamura <kirikonknow@gmail.com>
2020-03-29 14:20:59 +0900

    パーサーの作成

--- src/atcoder/beginners_selection/Abc086A.test.ts
+++ src/atcoder/beginners_selection/Abc086A.test.ts
@@ -1,4 +1,4 @@
-import { Abc086A, InputParser } from './Abc086A'
+import { Abc086A, Input, InputParser } from './Abc086A'
 
 describe('Abc086A 結合テスト', () => {
   const mockOutputPrinter = jest.fn(_ => _)
@@ -29,4 +29,66 @@ describe('Abc086A 結合テスト', () => {
     expect(mockOutputPrinter.mock.calls.length).toBe(1)
     expect(mockOutputPrinter.mock.calls[0][0]).toBe(expectedOutputString)
   }
+})
+
+describe('InputParser ユニットテスト', () => {
+  const parser = new InputParser()
+
+  describe('入力値を、整数a,bにパースする', () => {
+    describe('正常系', () => {
+      test.each([
+        ['3 4', new Input(3, 4)],
+        ['1 21', new Input(1, 21)]
+      ])('a,bを持つInputを返す', (inputString: string, expected: Input) => {
+        const actual = parser.parse(inputString)
+        expect(actual).toEqual(expected)
+      })
+    })
+
+    describe('入力値の形式が不正の場合、例外を投げる', () => {
+      test.each([
+        [''],
+        ['3'],
+        ['3 '],
+        [' 4']
+      ])('入力値の文字列を半角スペースで分割して2つ未満', (inputString: string) => {
+        expect(() => {
+          parser.parse(inputString)
+        }).toThrowError('input format is invalid.')
+      })
+
+      test.each([
+        ['a 4'],
+        ['3 b']
+      ])('分割した2つの要素のどちらかが数字ではない', (inputString: string) => {
+        expect(() => {
+          parser.parse(inputString)
+        }).toThrowError('input format is invalid.')
+      })
+    })
+
+    describe('a,bが制約に合っていない場合、例外を投げる', () => {
+      test.each([
+        ['0 4'],
+        ['-1 4'],
+        ['1.1 4'],
+        ['10001 4']
+      ])('aが1以上10000以下の整数ではない', (inputString: string) => {
+        expect(() => {
+          parser.parse(inputString)
+        }).toThrowError('a violates constraint.')
+      })
+
+      test.each([
+        ['3 0'],
+        ['3 -4'],
+        ['3 4.4'],
+        ['3 10004']
+      ])('bが1以上10000以下の整数ではない', (inputString: string) => {
+        expect(() => {
+          parser.parse(inputString)
+        }).toThrowError('b violates constraint.')
+      })
+    })
+  })
 })
--- src/atcoder/beginners_selection/Abc086A.ts
+++ src/atcoder/beginners_selection/Abc086A.ts
@@ -1,4 +1,26 @@
 export class Input {
+  private readonly a: number
+  private readonly b: number
+
+  constructor (a: number, b: number) {
+    if (!this.isValidNumberProperty(a)) {
+      throw new Error('a violates constraint.')
+    }
+    if (!this.isValidNumberProperty(b)) {
+      throw new Error('b violates constraint.')
+    }
+    this.a = a
+    this.b = b
+  }
+
+  private isValidNumberProperty (num: number): boolean {
+    if (!Number.isInteger(num)) {
+      return false
+    }
+
+    return num >= 1 && num <= 10000
+  }
+
   toOutputString (): string {
     return ''
   }
@@ -6,7 +28,25 @@ export class Input {
 
 export class InputParser {
   parse (inputString: string): Input {
-    return new Input()
+    const ab = inputString.split(' ')
+    if (ab.length < 2) {
+      throw new Error('input format is invalid.')
+    }
+
+    const a = this.parseNumberString(ab[0])
+    const b = this.parseNumberString(ab[1])
+    return new Input(a, b)
+  }
+
+  private parseNumberString (numberString: string): number {
+    if (numberString.length === 0) {
+      throw new Error('input format is invalid.')
+    }
+    const num = Number(numberString)
+    if (Number.isNaN(num)) {
+      throw new Error('input format is invalid.')
+    }
+    return num
   }
 }
 
