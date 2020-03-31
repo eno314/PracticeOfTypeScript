be5a7228da5eff08e354cae652c962127bccf795
Hiroto Kitamura <kirikonknow@gmail.com>
2020-03-31 09:17:59 +0900

    AtCoderで動くように入力値のフォーマットの条件を修正

--- src/atcoder/beginners_selection/Abc081A.test.ts
+++ src/atcoder/beginners_selection/Abc081A.test.ts
@@ -5,7 +5,7 @@ describe('入力値をマスs1,s2,s3にパースする', () => {
     ['100', new Squares('1', '0', '0')],
     ['010', new Squares('0', '1', '0')],
     ['001', new Squares('0', '0', '1')]
-  ])('InputParserのparseはSquaresを返す', (input: string, expected: Squares) => {
+  ])('parseInputはSquaresを返す', (input: string, expected: Squares) => {
     const actual = parseInput(input)
     expect(actual).toEqual(expected)
   })
@@ -15,9 +15,8 @@ describe('入力値の形式が不正の場合、例外を投げる', () => {
   test.each([
     [''],
     ['1'],
-    ['10'],
-    ['1001']
-  ])('InputParserのparseは、入力値の文字列長が3ではない場合、例外を投げる', (input: string) => {
+    ['10']
+  ])('parseInputは、入力値の文字列長が3未満の場合、例外を投げる', (input: string) => {
     expect(() => {
       parseInput(input)
     }).toThrowError('input format is invalid.')
@@ -28,7 +27,7 @@ describe('s1,s2,s3が制約に合致しない場合、例外を投げる', () =>
   test.each([
     [' 00'],
     ['211']
-  ])('InputParserのparseは、s1に当たる文字が0か1ではない場合、例外を投げる', (input: string) => {
+  ])('parseInputは、s1に当たる文字が0か1ではない場合、例外を投げる', (input: string) => {
     expect(() => {
       parseInput(input)
     }).toThrowError('input string violates constraint.')
@@ -37,7 +36,7 @@ describe('s1,s2,s3が制約に合致しない場合、例外を投げる', () =>
   test.each([
     ['0 0'],
     ['121']
-  ])('InputParserのparseは、s2に当たる文字が0か1ではない場合、例外を投げる', (input: string) => {
+  ])('parseInputは、s2に当たる文字が0か1ではない場合、例外を投げる', (input: string) => {
     expect(() => {
       parseInput(input)
     }).toThrowError('input string violates constraint.')
@@ -46,7 +45,7 @@ describe('s1,s2,s3が制約に合致しない場合、例外を投げる', () =>
   test.each([
     ['00 '],
     ['112']
-  ])('InputParserのparseは、s3に当たる文字が0か1ではない場合、例外を投げる', (input: string) => {
+  ])('parseInputは、s3に当たる文字が0か1ではない場合、例外を投げる', (input: string) => {
     expect(() => {
       parseInput(input)
     }).toThrowError('input string violates constraint.')
